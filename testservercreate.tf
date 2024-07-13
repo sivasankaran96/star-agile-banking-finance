560b33134ec5a8a127179df794bd06094a181365
sivasankaran <142564554+sivasankaran96@users.noreply.github.com>
2024-07-13 11:19:34 +0530

    Update testservercreate.tf

--- testservercreate.tf
+++ testservercreate.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   ami           = "ami-0c2af51e265bd5e0e"
   instance_type = "t2.medium"
-  key_name      = "shiva-mentor"
+  key_name      = "mykeypair"
 
   tags = {
     Name = "TestServer"
